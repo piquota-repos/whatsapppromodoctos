88486cd8238ffb4b6320bac891d6aa2210aa7a40
Lalitha <lalitha@piquota.com>
2025-07-19 22:09:13 +0530

    pradeepa next 500 contacts

--- scheduler.js
+++ scheduler.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const sendPromotionalMessages = require('./messageService');
 
 
-cron.schedule(' 56 20 * * *', async () => {
+cron.schedule(' 13 22 * * *', async () => {
   console.log("⏰Running scheduled WhatsApp promotional messages...");
   try {
     await sendPromotionalMessages();
