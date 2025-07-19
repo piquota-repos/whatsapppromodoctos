f771a1f364b7fc15ef7da621407a12e1eb0fff85
piquota-repo <nithya.raghavan@piquota.com>
2025-07-19 15:28:18 +0530

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const sendPromotionalMessages = require('./messageService');
 
 
-cron.schedule('25 15 * * *', async () => {
+cron.schedule('31 15 * * *', async () => {
   console.log("⏰Running scheduled WhatsApp promotional messages...");
   try {
     await sendPromotionalMessages();
