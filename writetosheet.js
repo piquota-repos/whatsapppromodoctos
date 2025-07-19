80c43a4caa96dec320c8e6381a3e55c124e397eb
Lalitha <lalitha@piquota.com>
2025-07-19 20:33:18 +0530

    keys changes

--- writetosheet.js
+++ writetosheet.js
@@ -5,7 +5,7 @@ const keys = JSON.parse(process.env.CREDENTIALS); // Google Service Account cred
 async function appendToSheet({ name, mobile, message }) {
     console.log("reached");
   const auth = new google.auth.GoogleAuth({
-    keys,
+     credentials: keys,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
 
